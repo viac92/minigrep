42b591b0ce4145409f3d4435e62076d7fd68661b
viac92 <64146594+viac92@users.noreply.github.com>
2022-05-24 15:28:20 +0200

    print error to stderr

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use minigrep::run;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
